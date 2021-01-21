d11ca85822d160c0752007179758ab588f85a84b
Francisco Vaz <ftomasvaz@gmail.com>
2021-01-21 21:53:29 +0000

    output posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { Post } from './post.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[ ] = [];
 
   constructor(private http: HttpClient) {}
 
@@ -46,7 +46,6 @@ export class AppComponent implements OnInit {
         return postsArray;
       }))
       .subscribe(posts => {
-        console.log(posts);
         this.loadedPosts = posts;
       });
   }
