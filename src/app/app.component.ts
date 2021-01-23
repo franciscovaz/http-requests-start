b4b6270d567c10e04954887e41f951a45f684110
Francisco Vaz <ftomasvaz@gmail.com>
2021-01-23 09:42:56 +0000

    getting the error from server with Subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Post } from './post.model';
 import { PostService } from './posts.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   loadedPosts: Post[ ] = [];
   isFetching: boolean = false;
   error = null;
+  private errorSub: Subscription;
 
   constructor(private postService: PostService) {}
 
   ngOnInit() {
+    this.errorSub = this.postService.error.subscribe(errorMessage => {
+      this.error = errorMessage;
+    })
+
+
     this.isFetching = true;
     this.postService.fetchPosts().subscribe(
       posts => {
@@ -53,6 +58,10 @@ export class AppComponent implements OnInit {
     this.postService.deletePosts().subscribe(() => {
       this.loadedPosts = [];
     })
+  }
+
+  ngOnDestroy() {
+    this.errorSub.unsubscribe();
   }
 
 }
--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,20 +1,27 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Subject } from "rxjs";
 import { map } from "rxjs/operators";
 import { Post } from "./post.model";
 
 @Injectable({ providedIn: 'root'})
 export class PostService {
+  error = new Subject<string>();
 
   constructor(private http: HttpClient){}
 
   createAndStorePost(title: string, content: string) {
     const postData: Post = {title, content};
-  // Send Http request
-  this.http.post<{name: string}>('https://angular-udemy-course-f2b18-default-rtdb.firebaseio.com/posts.json', postData)
-  .subscribe(response => {
-    console.log(response);
-  });
+    // Send Http request
+    this.http.post<{name: string}>('https://angular-udemy-course-f2b18-default-rtdb.firebaseio.com/posts.json', postData)
+    .subscribe(response => {
+      console.log(response);
+    },
+    error => {
+      this.error.next(error.message);
+    }
+
+    );
   }
 
   fetchPosts() {
